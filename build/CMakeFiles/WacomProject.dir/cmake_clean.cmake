01d9fa81c800b4960220fa1b48de3e6b0444f5b3
2020wmarvil <2020wmarvil@gmail.com>
2023-01-05 16:44:12 -0500

    Link against x11

---
+++ build/CMakeFiles/WacomProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WacomProject.dir/src/main.cpp.o"
+  "WacomProject"
+  "WacomProject.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WacomProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
